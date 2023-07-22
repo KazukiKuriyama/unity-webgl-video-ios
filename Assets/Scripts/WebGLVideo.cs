ca387bda42a0dfd3d5ddfac799b7444c2cb3b12d
kuriyama <64897247+KazukiKuriyama@users.noreply.github.com>
2023-07-22 23:53:30 +0900

    複数のゲームオブジェクトへのアタッチを想定 (#4)

--- Assets/Scripts/WebGLVideo.cs
+++ Assets/Scripts/WebGLVideo.cs
@@ -1,51 +1,81 @@
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Runtime.InteropServices;
 using System.Threading.Tasks;
 using AOT;
-using Unity.VisualScripting;
 using UnityEngine;
-using UnityEngine.Serialization;
-using UnityEngine.UI;
-using Object = UnityEngine.Object;
 
-public class MainScreen : MonoBehaviour
+public class WebGLVideo : MonoBehaviour
 {
     // 再生を開始するための関数
     [DllImport("__Internal")]
-    private static extern void StartVideo(string file, int time, Action<int> onStarted, Action<int> onStopped, int instanceID);
+    private static extern void StartVideo(string file, int time, Action<int> onStarted, Action<int> onStopped,
+        int instanceID);
 
     [DllImport("__Internal")]
-    private static extern void InitVideo(string file,int gameObjectID);
+    private static extern void InitVideo(string file, int gameObjectID);
 
     // 描画するための関数
     [DllImport("__Internal")]
-    private static extern void UpdateVideoTexture(int texture);
+    private static extern void UpdateVideoTexture(int texture,int gameObjectID);
 
     private static bool _isVideoInitialized = false;
     private static bool _isStartVideoInitialized = false;
 
-    [SerializeField] private string _videoFilePath;
+    [SerializeField] private bool _isUseStreamingAssets;
+
+    [SerializeField, Header("Relative URL from StreamingAssets or absolute path")]
+    private string _videoFileURL;
+
     private static bool _videoPlaying = false;
     private Texture2D _videoTexture;
     [SerializeField] private Material _targetMaterial;
     [SerializeField] private Texture _defaultTexture;
     private static List<int> _playingVideoGameObjectsID = new List<int>();
-
     private int _myGameObjectID;
 
+    /// <summary>
+    /// URLの初期設定
+    /// </summary>
+    /// <param name="videoFileURL"></param>
+    private void InitializeVideoFileURL(string videoFileURL)
+    {
+        if (_isUseStreamingAssets)
+            SetRelativeStreamingAssetURL(videoFileURL);
+        else
+            SetAbsoluteAssetURL(videoFileURL);
+    }
+
+    /// <summary>
+    /// 相対パス設定
+    /// </summary>
+    /// <param name="videoFileURL"></param>
+    /// <returns></returns>
+    public string SetRelativeStreamingAssetURL(string videoFileURL)
+    {
+        _isUseStreamingAssets = true;
+        _videoFileURL = Application.streamingAssetsPath + "/" + videoFileURL;
+        return _videoFileURL;
+    }
+
+    /// <summary>
+    /// 絶対パス設定
+    /// </summary>
+    /// <param name="videoFileURL"></param>
+    /// <returns></returns>
+    public string SetAbsoluteAssetURL(string videoFileURL)
+    {
+        _isUseStreamingAssets = false;
+        _videoFileURL = videoFileURL;
+        return _videoFileURL;
+    }
+    
     private void Awake()
     {
         _myGameObjectID = gameObject.GetInstanceID();
-        if (!_isStartVideoInitialized)
-        {
-            var videoPath = Application.streamingAssetsPath + "/" + _videoFilePath;
-            _isStartVideoInitialized = true;
-            InitVideo(videoPath);
-            Debug.Log("_isVideoInitialized = true;");
-            _isVideoInitialized = true;
-        }
+        InitializeVideoFileURL(_videoFileURL);
+        InitVideo(_videoFileURL, _myGameObjectID);
+        _isVideoInitialized = true;
     }
 
 
@@ -57,11 +87,7 @@ public class MainScreen : MonoBehaviour
     // 何かボタンが押されたら動画を再生する
     private void VideoTest()
     {
-        // bbb.mp4 の 1:52 から再生を開始する。
-        var videoPath = Application.streamingAssetsPath + "/" + _videoFilePath;
-        Debug.Log(videoPath);
-
-        StartVideo(videoPath, 0, OnStarted, OnStopped, _myGameObjectID);
+        StartVideo(_videoFileURL, 0, OnStarted, OnStopped, _myGameObjectID);
     }
 
     /// <summary>
@@ -95,7 +121,6 @@ public class MainScreen : MonoBehaviour
     {
         Debug.Log("OnStopped");
         _playingVideoGameObjectsID.Remove(gameObjectID);
-        
     }
 
     /// <summary>
@@ -124,14 +149,14 @@ public class MainScreen : MonoBehaviour
     {
         if (_isVideoInitialized && _playingVideoGameObjectsID.Contains(_myGameObjectID))
         {
-            Debug.Log("_isVideoInitialized && _playingVideoGameObjectsID.Contains(_myGameObjectID)");
             if (_videoTexture)
             {
                 Destroy(_videoTexture);
             }
 
-            _videoTexture = new Texture2D(1, 1, TextureFormat.ARGB32, false); // jslib側で再生成されるので空で良い
-            UpdateVideoTexture((int)_videoTexture.GetNativeTexturePtr());
+            // jslibでtextureを書き込み返却後に描画する
+            _videoTexture = new Texture2D(1, 1, TextureFormat.ARGB32, false);
+            UpdateVideoTexture((int)_videoTexture.GetNativeTexturePtr(),_myGameObjectID);
             _targetMaterial.mainTexture = _videoTexture;
         }
         else
